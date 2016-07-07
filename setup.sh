fd25dcdfe1bfe3ce6a17538973d2bb56998890ef
Serge Kovaleff <serge.kovaleff@gmail.com>
2016-07-07 15:23:13 +0000

    Install ag

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ sudo -H cp tempest.conf /etc/tempest/tempest.conf
 ls /etc/tempest
 cat /etc/tempest/tempest.conf | grep uri
 echo "-->Tempest"
+echo "-->ag"
+sudo apt install silversearcher-ag
+echo "--<"
